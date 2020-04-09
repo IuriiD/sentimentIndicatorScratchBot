fd15182ca59ba6ab9b1f9e4ac6cceaa11f49519d
Iurii Dziuban <iurii.dziuban@gmail.com>
2020-04-09 17:18:04 +0300

    Initial commit

---
+++ sentiment_fetcher.js
@@ -0,0 +1,47 @@
+(function (ext) {
+    var APIKEY = 'YOUR_GOOGLE_NATURAL_LANGUAGE_API_KEY';
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function () { };
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function () {
+        return { status: 2, msg: 'Ready' };
+    };
+
+    ext.get_temp = function (userInput, callback) {
+        console.log('LOADED!');
+        // Make an AJAX call to the Google Natural Language API
+        $.ajax({
+            type: "POST",
+            url: "https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + APIKEY,
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                "document": {
+                    "type": "PLAIN_TEXT",
+                    "content": userInput
+                },
+                "encodingType": "UTF8",
+            }),
+            success: function (sentiment_data) {                
+                // Got the data - parse it and return sentimentScore and sentimentMagnitude in format '+0.4|+0.3'
+                var sentimentScore = sentiment_data['documentSentiment']['score'] || '+0.0';
+                var sentimentMagnitude = sentiment_data['documentSentiment']['magnitude'] || '0.0';
+                var response = `${sentimentScore > 0 ? '+' : ''}${sentimentScore}|+${sentimentMagnitude}`
+                console.log('sentimentScore, sentimentMagnitude = ', response);
+                callback(response);
+            }
+        });
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'users_input %s', 'get_temp', 'what a nice day'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('GetSentiment Extension', descriptor, ext);
+})({});
