6c35883b3246797aa813f7dd765be5c6fe9d607b
Iurii Dziuban <iurii.dziuban@gmail.com>
2020-04-10 17:12:38 +0300

    Minor updates

--- sentiment_fetcher.js
+++ sentiment_fetcher.js
@@ -1,4 +1,4 @@
-(function (ext) {
+(function (ext) {    
     var APIKEY = 'YOUR_GOOGLE_NATURAL_LANGUAGE_API_KEY';
 
     // Cleanup function when the extension is unloaded
@@ -10,26 +10,28 @@
         return { status: 2, msg: 'Ready' };
     };
 
-    ext.get_temp = function (userInput, callback) {
-        console.log('LOADED!');
+    ext.get_sentiment = function (userInput, callback) {
+        if (APIKEY === 'YOUR_GOOGLE_NATURAL_LANGUAGE_API_KEY') callback('NO API KEY');
         // Make an AJAX call to the Google Natural Language API
         $.ajax({
-            type: "POST",
-            url: "https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + APIKEY,
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({
-                "document": {
-                    "type": "PLAIN_TEXT",
-                    "content": userInput
-                },
-                "encodingType": "UTF8",
-            }),
-            success: function (sentiment_data) {                
-                // Got the data - parse it and return sentimentScore and sentimentMagnitude in format '+0.4|+0.3'
+            type: 'POST',
+            url: 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + APIKEY,
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(
+                {
+                    "document": {
+                        "type": "PLAIN_TEXT",
+                        "language": "EN",
+                        "content": userInput
+                    },
+                    "encodingType": "UTF8"
+                }
+            ),
+            success: function (sentiment_data) {
+                // Got the data - parse it and return sentiment score and magnitude
                 var sentimentScore = sentiment_data['documentSentiment']['score'] || '+0.0';
                 var sentimentMagnitude = sentiment_data['documentSentiment']['magnitude'] || '0.0';
-                var response = `${sentimentScore > 0 ? '+' : ''}${sentimentScore}|+${sentimentMagnitude}`
-                console.log('sentimentScore, sentimentMagnitude = ', response);
+                var response = `${sentimentScore > 0 ? '+' : ''}${sentimentScore}|+${sentimentMagnitude}`;
                 callback(response);
             }
         });
@@ -38,7 +40,7 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'users_input %s', 'get_temp', 'what a nice day'],
+            ['R', 'user input %s', 'get_sentiment', 'what a nice day'],
         ]
     };
 
